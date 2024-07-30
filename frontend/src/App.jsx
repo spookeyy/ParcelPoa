792eafe36fba3efdcd2c4cbacec350e48a21fa04
petermbugua <pmbugua276@gmail.com>
2024-07-31 01:00:59 +0300

    redone most the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Invoice from './pages/Invoice';
 import Messages_List from './pages/Messages_List';
+
 import Messages_Details from './pages/Messages_Details';
 import Order_List from './pages/Order_List';
 import Order_Details from './pages/Order_Details';
@@ -22,14 +23,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/create-account" element={<Create_Account />} />
         <Route path="/signup" element={<Create_Account />} />
         <Route path="/agent-list" element={<Agent_List />} />
         <Route path="/agent-requests" element={<Agent_Requests />} />
-        <Route path="/invoice" element={<Invoice />} />
+        <Route path="/invoice/:id" element={<Invoice />} />
         <Route path="/messages-list" element={<Messages_List />} />
-        <Route path="/messages-details" element={<Messages_Details />} />
+        <Route path="/message/:id" element={<Messages_Details />} />
         <Route path="/order-list" element={<Order_List />} />
-        <Route path="/order-details" element={<Order_Details />} />
+        <Route path="/order/:id" element={<Order_Details />} />
         <Route path="/reset-password" element={<Reset_Password />} />
       </Routes>
       <Footer />
