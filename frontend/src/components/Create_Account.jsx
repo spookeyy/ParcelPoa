6c8da78fa1e7b178d31fd4cf9fe8ea1e388b47ac
spookeyy <pangasmeshack@gmail.com>
2024-08-02 23:44:42 +0300

    login register

--- frontend/src/components/Create_Account.jsx
+++ frontend/src/components/Create_Account.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { UserContext } from '../../Context/UserContext';
+import { UserContext } from '../Context/UserContext';
 import { Link, useNavigate } from 'react-router-dom';
 import {toast} from 'react-toastify';
 import { EyeIcon, EyeOffIcon } from "lucide-react";
