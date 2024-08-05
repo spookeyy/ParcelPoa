9a3b18f60bea33c24a9b6bc0f73451d96bf6a80b
spookeyy <pangasmeshack@gmail.com>
2024-08-05 11:17:41 +0300

    modified

--- backend/models.py
+++ backend/models.py
@@ -19,6 +19,7 @@ class User(db.Model):
     updated_at = Column(TIMESTAMP, nullable=False, default=datetime.now, onupdate=datetime.now)
     password_hash = Column(String(128), nullable=False)
     profile_picture = Column(String, default='default.png', nullable=True)
+    status = Column(Enum('Active', 'Inactive', name='user_statuses'), default='Active', nullable=False)
 
     parcels = relationship('Parcel', back_populates='sender', foreign_keys='Parcel.sender_id')
     deliveries = relationship('Delivery', back_populates='agent', foreign_keys='Delivery.agent_id')
